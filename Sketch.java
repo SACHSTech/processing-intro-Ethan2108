7c10c44bf2298e0a09f2f6c6118352676ff13ed4
Ethan Au <75390842+Ethan2108@users.noreply.github.com>
2022-02-22 16:36:24 +0000

    Added background

--- Sketch.java
+++ Sketch.java
@@ -16,7 +16,7 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(0, 255, 255);
   }
 
   /**
@@ -24,12 +24,7 @@ public class Sketch extends PApplet {
    */
   public void draw() {
 	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
-
-    stroke(255);
-    line(50, 125, 70, 50);  
+	// 
   }
   
   // define other methods down here.
